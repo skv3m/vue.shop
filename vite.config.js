ac453eef0c5f475beb785af90d6ca934bd430e4f
skv3m <ivanryabov007@gmail.com>
2024-05-17 13:20:37 +0400

    addbot

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,40 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { exec } from 'child_process'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+// Define a custom plugin to start the bot
+function startBotPlugin() {
+  return {
+    name: 'start-bot-plugin',
+    configResolved() {
+      // Start the bot when the config is resolved
+      exec('node src/tgbot.mjs', (err, stdout, stderr) => {
+        if (err) {
+          console.error(`Ошибка при запуске бота: ${err.message}`);
+          return;
+        }
+        if (stderr) {
+          console.error(`stderr: ${stderr}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+      });
+    }
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), startBotPlugin()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    port: 5173,
+    open: true,
   }
 })
